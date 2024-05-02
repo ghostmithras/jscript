c8c96c0b9f5b5d558c41545b54107ea92212a257
Brett Still <bstill62@academic.rrc.ca>
2024-05-02 09:53:16 -0500

    Chapter 2 partial

---
+++ chapter2/sandbox.js
@@ -0,0 +1,124 @@
+/*let age = 25;
+let year = 2019;
+
+// log things to console
+console.log(age, year);
+
+age = 30;
+
+console.log(age);
+
+const points = 100;
+
+console.log(points);
+
+var score = 75;
+
+console.log(score);*/
+
+/*
+// strings
+console.log('Hello World');
+
+let email = 'mario@thenetninja@co.uk';
+
+console.log(email);
+
+// string concatenation
+let firstName = 'Brandon';
+let lastName = 'Sanderson';
+let fullName = firstName + ' ' + lastName;
+
+console.log(fullName);
+
+// getting characters
+console.log(fullName[0]);
+
+// string length
+console.log(fullName.length);
+
+// string methods
+console.log(fullName.toUpperCase());
+let result = fullName.toLowerCase();
+console.log(result, fullName);
+
+let index = email.indexOf('@');
+console.log(index);
+
+// common string methods
+result = email.lastIndexOf('n');
+console.log(result);
+
+result = email.slice(0, 5);
+console.log(result);
+
+result = email.substr(6, 6);
+console.log(result);
+
+result = email.replace('m', 'w');
+console.log(result);
+
+result = email.replace('n', 'w');
+console.log(result);
+*/
+
+/*
+let radius = 10;
+const pi = 3.14;
+
+console.log(radius, pi);
+
+// math operations + - * / ** %
+
+console.log(10 / 2);
+let result = radius % 3;
+console.log(result);
+result = pi * radius ** 2;
+console.log(result);
+
+// order of operations - BEDMAS
+
+result = 5 * (10 - 3) ** 2;
+console.log(result);
+
+let likes = 10;
+likes++;
+console.log(likes);
+likes--;
+console.log(likes);
+likes += 10;
+console.log(likes);
+likes *= 2;
+console.log(likes);
+likes -= 10;
+console.log(likes);
+likes /= 3;
+console.log(likes);
+
+// NaN
+
+console.log(5 / 'hello');
+console.log(5 * 'hello');
+
+result = 'the blog has ' + likes + ' likes.'
+console.log(result);
+*/
+
+/*
+const title = 'Best reads of 2019';
+const author = 'Mario';
+const likes = 30;
+
+// concatenation way
+let result = 'The blog called ' + title + ' by ' + author + ' has ' + likes + ' likes.';
+console.log(result);
+
+// template string way
+result = `The blog called ${title} by ${author} has ${likes} likes.`;
+console.log(result);
+
+// creating html templates
+let html = `<h2>${title}</h2> <p>by ${author}</p> <span>This blog has ${likes} likes.</span>`;
+console.log(html);
+*/
+
